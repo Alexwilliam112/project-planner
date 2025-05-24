f9e5ce6180b6cb7bc89db257b8097304b75ae7b8
Abdullah <abdul.abdullah@mekari.com>
2025-05-24 19:21:26 +0700

    feat: update and delete projects

--- client/src/services/projects/projects-endpoints.mjs
+++ client/src/services/projects/projects-endpoints.mjs
@@ -3,6 +3,9 @@
 const PROJECTS_API = {
   GET_ALL: '/ops-center/project/v1/index',
   CREATE: '/ops-center/project/v1/create',
+  UPDATE: '/ops-center/project/v1/update',
+  UPDATE_PIC: '/ops-center/project/v1/update-pic',
+  DELETE: '/ops-center/project/v1/delete',
 }
 
 export { PROJECTS_API }
--- client/src/services/projects/projects-service.mjs
+++ client/src/services/projects/projects-service.mjs
@@ -22,8 +22,28 @@ const projectsService = {
 
     return data
   },
-  async delete(params) {
-    const response = await apiClient.delete(PROJECTS_API.CREATE, { params })
+  async update(id_project, payload) {
+    const response = await apiClient.post(PROJECTS_API.UPDATE, payload, { params: { id_project } })
+
+    const { error, message, data } = response.data
+
+    if (error) throw new Error(message)
+
+    return data
+  },
+  async updatePIC(id_project, payload) {
+    const response = await apiClient.post(PROJECTS_API.UPDATE_PIC, payload, {
+      params: { id_project },
+    })
+
+    const { error, message, data } = response.data
+
+    if (error) throw new Error(message)
+
+    return data
+  },
+  async delete(id_project) {
+    const response = await apiClient.delete(PROJECTS_API.DELETE, { params: { id_project } })
 
     const { error, message, data } = response.data
 
