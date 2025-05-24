1b191f3e36d3fb0c6bc4875706b8f72c3f6a7994
Alexander William <alexwilliam112@gmail.com>
2025-05-24 20:27:16 +0700

    feat: added gantt api

--- client/src/services/gantt-chart/gantt-endpoints.mjs
+++ client/src/services/gantt-chart/gantt-endpoints.mjs
@@ -2,7 +2,7 @@
 
 const GANTT_API = {
   GET_ALL: '/ops-center/project/v1/index-gantt',
-  GET_TASK: '/ops-center/project/v1/get-task',
+  GET_TASK: '/ops-center/project/v1/index-task',
 }
 
 export { GANTT_API }
