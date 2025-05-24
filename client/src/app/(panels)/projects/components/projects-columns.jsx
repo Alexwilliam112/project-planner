5a2fdeb3c28bf25637ef7201ef918496e871a971
Abdullah <abdul.abdullah@mekari.com>
2025-05-24 23:28:31 +0700

    feat: table UI enhancement

--- client/src/app/(panels)/projects/components/projects-columns.jsx
+++ client/src/app/(panels)/projects/components/projects-columns.jsx
@@ -16,44 +16,62 @@ export const projectsColumns = [
   {
     accessorKey: 'name',
     header: ({ column }) => <DataTableColumnHeader column={column} title="Project Name" />,
+    cell: ({ row }) => <div className="w-[20ch] py-2 text-wrap">{row.original.name}</div>,
   },
   {
     accessorKey: 'product_id.name',
     header: ({ column }) => <DataTableColumnHeader column={column} title="Project Type" />,
+    cell: ({ row }) => (
+      <div className="w-[20ch] py-2 text-wrap">{row.original.product_id.name}</div>
+    ),
   },
   {
     accessorKey: 'category_id.name',
     header: ({ column }) => <DataTableColumnHeader column={column} title="Project Category" />,
+    cell: ({ row }) => (
+      <div className="w-[20ch] py-2 text-wrap">{row.original.category_id.name}</div>
+    ),
   },
   {
     accessorKey: 'date_start',
     header: ({ column }) => <DataTableColumnHeader column={column} title="Start Date" />,
-    cell: ({ row }) => format(row.original.date_start, 'PPP'),
+    cell: ({ row }) => (
+      <div className="w-[20ch] py-2 text-wrap">{format(row.original.date_start, 'PPP')}</div>
+    ),
   },
   {
     accessorKey: 'end_date',
     header: ({ column }) => <DataTableColumnHeader column={column} title="End Date" />,
-    cell: ({ row }) => format(row.original.date_end, 'PPP'),
+    cell: ({ row }) => (
+      <div className="w-[20ch] py-2 text-wrap">{format(row.original.date_end, 'PPP')}</div>
+    ),
   },
   {
     accessorKey: 'project_owner_id.name',
     header: ({ column }) => <DataTableColumnHeader column={column} title="Project Owner" />,
+    cell: ({ row }) => (
+      <div className="w-[20ch] py-2 text-wrap">{row.original.project_owner_id.name}</div>
+    ),
   },
   {
     accessorKey: 'status_id.name',
     header: ({ column }) => <DataTableColumnHeader column={column} title="Project Status" />,
     cell: ({ row }) => (
-      <Badge className={cn(`bg-[${row.original.status_id.color}]`)}>
-        {row.original.status_id.name}
-      </Badge>
+      <div className="w-[20ch] py-2 text-wrap">
+        <Badge className={cn(`bg-[${row.original.status_id.color}]`)}>
+          {row.original.status_id.name}
+        </Badge>
+      </div>
     ),
   },
   {
     accessorKey: 'progress',
     header: 'Progress',
+    cell: ({ row }) => <div className="w-[20ch] py-2 text-wrap">{row.original.progress}</div>,
   },
   {
     accessorKey: 'note',
     header: 'Note',
+    cell: ({ row }) => <div className="w-[40ch] py-2 text-wrap">{row.original.note}</div>,
   },
 ]
--- client/src/components/table/column-header.jsx
+++ client/src/components/table/column-header.jsx
@@ -17,7 +17,7 @@ export function DataTableColumnHeader({ column, title, className }) {
   }
 
   return (
-    <div className={cn('flex items-center space-x-2', className)}>
+    <div className={cn('flex items-center space-x-2 py-3', className)}>
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button variant="ghost" size="sm" className="-ml-3 h-8 data-[state=open]:bg-accent">
