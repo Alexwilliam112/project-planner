506c5d0eec780091a0ce30bc09896f3c05cd80ae
Alexander William <alexwilliam112@gmail.com>
2025-05-31 11:51:57 +0700

    feat: added badges in task-summary

--- client/src/app/(panels)/task-summary/_components/task-summary-columns.jsx
+++ client/src/app/(panels)/task-summary/_components/task-summary-columns.jsx
@@ -1,13 +1,15 @@
-'use client'
+"use client";
 
-import { format } from 'date-fns'
-import { Pencil, Trash2 } from 'lucide-react'
-import { Button } from '@/components/ui/button'
+import { format } from "date-fns";
+import { Pencil, Trash2 } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
+
 
 export const createTaskSummaryColumns = ({ handleEdit }) => [
   {
-    id: 'actions',
-    header: 'Actions',
+    id: "actions",
+    header: "Actions",
     cell: ({ row }) => (
       <Button
         className="w-8 h-8"
@@ -20,54 +22,107 @@ export const createTaskSummaryColumns = ({ handleEdit }) => [
     ),
   },
   {
-    accessorKey: 'assignee_id.name',
-    header: 'Assignee',
+    accessorKey: "assignee_id.name",
+    header: "Assignee",
   },
   {
-    accessorKey: 'project_id.name',
-    header: 'Project',
+    accessorKey: "project_id.name",
+    header: "Project",
   },
   {
-    accessorKey: 'milestone_id.name',
-    header: 'Milestone',
-  },
-  {
-    accessorKey: 'name',
-    header: 'Task Name',
-  },
-  {
-    accessorKey: 'priority_id.name',
-    header: 'Priority',
-  },
-  {
-    accessorKey: 'status_id.name',
-    header: 'Status',
-  },
-  {
-    id: 'date_range',
-    header: 'Date',
+    accessorKey: "milestone_id.name",
+    header: "Milestone",
     cell: ({ row }) => (
-      <div className="py-2">
-        {format(row.original.date_start, 'PPP')} - {format(row.original.date_end, 'PPP')}
+      <div className="w-[15ch] py-2 text-wrap">
+        <Badge
+          style={{
+            backgroundColor: row.original.milestone_id.color,
+            color: "#fff",
+          }}
+        >
+          {row.original.milestone_id.name}
+        </Badge>
       </div>
     ),
   },
   {
-    accessorKey: 'deadline',
-    header: 'Deadline',
-    cell: ({ row }) => format(row.original.deadline, 'PPP'),
+    accessorKey: "name",
+    header: "Task Name",
   },
   {
-    accessorKey: 'est_mh',
-    header: 'Est MH',
+    accessorKey: "priority_id.name",
+    header: "Priority",
+    cell: ({ row }) => (
+      <div className="w-[10ch] py-2 text-wrap">
+        <Badge
+          style={{
+            backgroundColor: row.original.priority_id.color,
+            color: "#fff",
+          }}
+        >
+          {row.original.priority_id.name}
+        </Badge>
+      </div>
+    ),
   },
   {
-    accessorKey: 'progress',
-    header: 'Progress',
-    cell: ({ row }) => (row.original.progress ? row.original.progress + '%' : 0 + '%'),
+    accessorKey: "status_id.name",
+    header: "Status",
+    cell: ({ row }) => (
+      <div className="w-[15ch] py-2 text-wrap">
+        <Badge
+          style={{
+            backgroundColor: row.original.status_id.color,
+            color: "#fff",
+          }}
+        >
+          {row.original.status_id.name}
+        </Badge>
+      </div>
+    ),
   },
   {
-    accessorKey: 'note',
-    header: 'Note',
+    id: "date_range",
+    header: "Date",
+    cell: ({ row }) => (
+      <div className="py-2">
+        {new Date(row.original.date_start).toLocaleString("id-ID", {
+          timeZone: "Asia/Jakarta",
+          day: "2-digit",
+          month: "short",
+          year: "numeric",
+          hour: "2-digit",
+          minute: "2-digit",
+        })}{" "}
+        -{" "}
+        {new Date(row.original.date_end).toLocaleString("id-ID", {
+          timeZone: "Asia/Jakarta",
+          day: "2-digit",
+          month: "short",
+          year: "numeric",
+          hour: "2-digit",
+          minute: "2-digit",
+        })}{" "}
+      </div>
+    ),
   },
-]
+  {
+    accessorKey: "deadline",
+    header: "Deadline",
+    cell: ({ row }) => format(row.original.deadline, "PPP"),
+  },
+  {
+    accessorKey: "est_mh",
+    header: "Est MH",
+  },
+  {
+    accessorKey: "progress",
+    header: "Progress",
+    cell: ({ row }) =>
+      row.original.progress ? row.original.progress + "%" : 0 + "%",
+  },
+  {
+    accessorKey: "note",
+    header: "Note",
+  },
+];
