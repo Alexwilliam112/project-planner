2ea7a2be325128f8279fbb93119b28d99845f9ab
Abdullah <abdul.abdullah@mekari.com>
2025-06-20 16:24:27 +0700

    feat: null safety

--- client/src/app/(panels)/task-summary/_components/resource-capacity-columns.jsx
+++ client/src/app/(panels)/task-summary/_components/resource-capacity-columns.jsx
@@ -1,104 +1,104 @@
-"use client";
-import { Badge } from "@/components/ui/badge";
+'use client'
+import { Badge } from '@/components/ui/badge'
 
 export const resourceCapacityColumns = [
   {
-    accessorKey: "division",
-    header: "Division",
+    accessorKey: 'division',
+    header: 'Division',
   },
   {
-    accessorKey: "squad",
-    header: "Squad",
+    accessorKey: 'squad',
+    header: 'Squad',
   },
   {
-    accessorKey: "name",
-    header: "Resource Name",
+    accessorKey: 'name',
+    header: 'Resource Name',
     cell: ({ row }) => <div className="py-2">{row.original.name}</div>,
   },
   {
-    accessorKey: "resource_type",
-    header: "Role",
+    accessorKey: 'resource_type',
+    header: 'Role',
   },
   {
-    accessorKey: "work_days",
-    header: "Work Days",
+    accessorKey: 'work_days',
+    header: 'Work Days',
   },
   {
-    accessorKey: "time_off",
-    header: "Time Off",
+    accessorKey: 'time_off',
+    header: 'Time Off',
   },
   {
-    accessorKey: "nett_work_days",
-    header: "Nett Work Days",
+    accessorKey: 'nett_work_days',
+    header: 'Nett Work Days',
   },
   {
-    accessorKey: "total_mh",
-    header: "Total MH",
+    accessorKey: 'total_mh',
+    header: 'Total MH',
   },
   {
-    accessorKey: "current_mh",
-    header: "Current MH",
+    accessorKey: 'current_mh',
+    header: 'Current MH',
     cell: ({ row }) => (
       <div className="w-[7ch] py-2 text-wrap">
-        {Number(row.original.capacity.split(" ")[0]) >= 95 ? (
+        {Number(row.original.capacity.split(' ')[0]) >= 95 ? (
           <Badge
             style={{
-              backgroundColor: "#2DBB09",
-              color: "#fff",
+              backgroundColor: '#2DBB09',
+              color: '#fff',
             }}
           >
-            {row.original.current_mh.toFixed(2)}
+            {row.original.current_mh?.toFixed(2) || 'null'}
           </Badge>
         ) : (
           <Badge
             style={{
-              backgroundColor: "#A20808",
-              color: "#fff",
+              backgroundColor: '#A20808',
+              color: '#fff',
             }}
           >
-            {row.original.current_mh.toFixed(2)}
+            {row.original.current_mh?.toFixed(2) || 'null'}
           </Badge>
         )}
       </div>
     ),
   },
   {
-    accessorKey: "idle_mh",
-    header: "Idle MH",
+    accessorKey: 'idle_mh',
+    header: 'Idle MH',
     cell: ({ row }) => (
       <div className="w-[7ch] py-2 text-wrap">
-        {Number(row.original.capacity.split(" ")[0]) >= 95 ? (
+        {Number(row.original.capacity.split(' ')[0]) >= 95 ? (
           <Badge
             style={{
-              backgroundColor: "#2DBB09",
-              color: "#fff",
+              backgroundColor: '#2DBB09',
+              color: '#fff',
             }}
           >
-            {row.original.idle_mh.toFixed(2)}
+            {row.original.idle_mh?.toFixed(2) || 'null'}
           </Badge>
         ) : (
           <Badge
             style={{
-              backgroundColor: "#A20808",
-              color: "#fff",
+              backgroundColor: '#A20808',
+              color: '#fff',
             }}
           >
-            {row.original.idle_mh.toFixed(2)}
+            {row.original.idle_mh?.toFixed(2) || 'null'}
           </Badge>
         )}
       </div>
     ),
   },
   {
-    accessorKey: "capacity",
-    header: "Capacity",
+    accessorKey: 'capacity',
+    header: 'Capacity',
     cell: ({ row }) => (
       <div className="w-[15ch] py-2 text-wrap">
-        {Number(row.original.capacity.split(" ")[0]) >= 95 ? (
+        {Number(row.original.capacity.split(' ')[0]) >= 95 ? (
           <Badge
             style={{
-              backgroundColor: "#2DBB09",
-              color: "#fff",
+              backgroundColor: '#2DBB09',
+              color: '#fff',
             }}
           >
             {row.original.capacity}
@@ -106,8 +106,8 @@ export const resourceCapacityColumns = [
         ) : (
           <Badge
             style={{
-              backgroundColor: "#A20808",
-              color: "#fff",
+              backgroundColor: '#A20808',
+              color: '#fff',
             }}
           >
             {row.original.capacity}
@@ -116,4 +116,4 @@ export const resourceCapacityColumns = [
       </div>
     ),
   },
-];
+]
